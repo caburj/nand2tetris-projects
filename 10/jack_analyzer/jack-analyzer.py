f4fa4c7c9a09d59fd51de23476246d1065bd3413
Joseph Caburnay <caburnay.joseph@gmail.com>
2019-04-14 12:29:19 +0200

    Refactor analyzer.

--- 10/jack_analyzer/jack-analyzer.py
+++ 10/jack_analyzer/jack-analyzer.py
@@ -9,27 +9,32 @@ def jack_analyzer():
     pass
 
 
+def files_to_process(path: Path):
+    """Returns the path of the jack files in the given path if dir."""
+    if path.is_dir():
+        return [d for d in path.iterdir() if d.suffix == ".jack"]
+    else:
+        return [path] if path.suffix == ".jack" else []
+
+
 @jack_analyzer.command()
 @click.argument("path", type=click.Path(exists=True))
 def tokenize(path):
-    """tokenizer cli"""
-    path = Path(path)
-    if path.is_dir():
-        filenames = [d for d in path.iterdir() if d.suffix == ".jack"]
-    else:
-        filenames = [path]
+    """tokenizer"""
+    filenames = files_to_process(Path(path))
+    if not filenames:
+        click.echo(f"Unable to detect jack files in the given path: {path}")
+        return
     for fname in filenames:
         with open(fname) as stream:
-            string = stream.read()
-            tokens_list = tokenizer.tokenize(string)
+            tokens_list = tokenizer.tokenize(stream.read())
             result_string = (
                 "<tokens>\n"
                 + "\n".join(map(lambda t: f"<{t[0]}> {t[1]} </{t[0]}>", tokens_list))
                 + "\n</tokens>\n"
             )
-            parent_dir = fname.parent
-            new_name = fname.stem + "T"
-            result_fname = (parent_dir / new_name).with_suffix(".xml")
+            # append 'T' to the stem
+            result_fname = (fname.parent / (fname.stem + "T")).with_suffix(".xml")
             with open(result_fname, "w") as f:
                 f.write(result_string)
 
@@ -37,19 +42,17 @@ def tokenize(path):
 @jack_analyzer.command()
 @click.argument("path", type=click.Path(exists=True))
 def parse(path):
-    """parser cli"""
-    path = Path(path)
-    if path.is_dir():
-        filenames = [d for d in path.iterdir() if d.suffix == ".jack"]
-    else:
-        filenames = [path]
+    """parser"""
+    filenames = files_to_process(Path(path))
+    if not filenames:
+        click.echo(f"Unable to detect jack files in the given path: {path}")
+        return
     for fname in filenames:
         with open(fname) as stream:
-            source_code_string = stream.read()
-            result = parser.parse(source_code_string)
-            string = "\n".join(result) + "\n"
+            result = parser.parse(stream.read())
+            result_string = "\n".join(result) + "\n"
             with open(fname.with_suffix(".xml"), "w") as ans:
-                ans.write(string)
+                ans.write(result_string)
 
 
 if __name__ == "__main__":
